1caba6db9589896bced1b05ff02e4bcdd154f49b
mareerray <mareerray@gmail.com>
2025-09-05 16:41:26 +0300

    Add Piscine Checkpoint execise solution

---
+++ AgeFinder/src/AgeFinder.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class AgeFinder {
+    public int calculateAge(String date) {
+        try {
+            LocalDate startDate = LocalDate.parse(date);
+            LocalDate now =  LocalDate.now();
+
+            Period p = Period.between(startDate, now);
+
+            int years = p.getYears();
+            return years;
+
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+}
